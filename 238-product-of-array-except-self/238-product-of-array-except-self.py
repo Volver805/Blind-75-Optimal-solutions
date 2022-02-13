1e8b0f7717a87282b71963daf1109f296e97d989
Ahmed Sayed <37520614+Volver805@users.noreply.github.com>
2022-02-13 21:35:10 +0200

    Time: 333 ms (43.56%), Space: 20.6 MB (99.88%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -1,14 +1,13 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        l = [1] * len(nums)
-        r = [1] * len(nums)
         size = len(nums)
-        lC = rC = 1
-        for i in range(size):
-            l[i] = lC
-            lC *= nums[i]
-            r[size-1-i] = rC
-            rC *= nums[size-1-i]
-        for i in range(len(nums)):
-            nums[i] = l[i] * r[i]
+        # We can consider this solution O(1) as this problem states 
+        # that the output array does not count as an extra space.
+        res = [1]*size
+        for i in range(1, size):
+            res[i] = nums[i-1] * res[i-1]
+        R = nums[-1]
+        for i in range(size-2, -1, -1):
+            nums[i], R = R*res[i], nums[i] * R
+        nums[-1] = 1 * res[-1]
         return nums
