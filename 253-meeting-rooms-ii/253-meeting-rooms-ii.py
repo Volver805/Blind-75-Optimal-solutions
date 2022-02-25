b2e0b4887dd4379f4532bac66429a555033772cd
Ahmed Sayed <37520614+Volver805@users.noreply.github.com>
2022-02-25 03:16:54 +0200

    Time: 120 ms (43.67%), Space: 17.4 MB (88.33%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -1,15 +1,11 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        rooms = []
-        intervals.sort(key=lambda x: x[0])
-        for start, end in intervals:
-            l = True
-            print(rooms)
-            for i in range(len(rooms)):
-                if rooms[i] <= start:
-                    rooms[i] = end
-                    l=False
-                    break
-            if l:
-                rooms.append(end)
-        return len(rooms)
+        intervals.sort(key=lambda x:x[0])
+        h = [intervals[0][1]] # heap contains the end of rooms
+        for interval in intervals[1:]:
+            # Compare the end of the least waiting room with the current interval start
+            if interval[0] >= h[0]:
+                heapq.heappop(h)
+            heapq.heappush(h, interval[1])
+        return len(h)
+            
