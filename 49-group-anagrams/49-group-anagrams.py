49e548c7f2e0fedd379071ddaba2595cba1fa09d
Ahmed Sayed <37520614+Volver805@users.noreply.github.com>
2022-02-28 02:58:42 +0200

    Time: 187 ms (23.78%), Space: 19.9 MB (16.99%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,19 +1,9 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]):
-        occur = []
-        res = []
+        res = defaultdict(list)
         for string in strs:
-            t = {}
+            counter = [0] * 26
             for ch in string:
-                t[ch] = t.get(ch, 0) + 1
-            self.appendGroup(res, occur, t, string)
-        return res
-
-                    
-    def appendGroup(self, res:list, occur:list, t:dict, string:str):
-        for i in range(len(occur)):
-            if occur[i] == t:
-                res[i].append(string)
-                return
-        occur.append(t)
-        res.append([string])
+                counter[ord(ch) - ord('a')] += 1
+            res[tuple(counter)].append(string)
+        return res.values()
