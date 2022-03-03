e080d3d467ebedf7b9d7a11cbd9fda6ce00988df
Ahmed Sayed <37520614+Volver805@users.noreply.github.com>
2022-03-03 18:43:38 +0200

    Time: 4812 ms (91.97%), Space: 13.9 MB (73.01%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -20,15 +20,3 @@ class Solution:
                 if self.search(word[1:], board, newRow, newCol):
                     return True
         board[i][j] = ch
-#         if i < 0 or j < 0 or i == len(board) or j == len(board[i]): return
-#         if (i, j) in visited: return
-#         if word[0] != board[i][j]: return
-#         if len(word) == 1:
-#             return True
-#         visited.add((i, j))
-#         top = self.search(word[1:], board, i-1, j, visited.copy())
-#         bottom = self.search(word[1:], board, i, j-1, visited.copy())
-#         left = self.search(word[1:], board, i+1, j, visited.copy())
-#         right = self.search(word[1:], board, i, j+1, visited.copy())
-        
-#         return top or bottom or left or right
