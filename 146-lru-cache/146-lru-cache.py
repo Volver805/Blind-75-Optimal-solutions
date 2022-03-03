15ba30abf3ea86788141be665678b99a81e1a2ae
Ahmed Sayed <37520614+Volver805@users.noreply.github.com>
2022-03-03 02:18:17 +0200

    Time: 1491 ms (26.11%), Space: 75 MB (89.65%) - LeetHub

--- 146-lru-cache/146-lru-cache.py
+++ 146-lru-cache/146-lru-cache.py
@@ -1,25 +1,56 @@
+class LinkedList:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+        
 class LRUCache:
-
     def __init__(self, capacity: int):
-        self.size = capacity
-        self.list = {}
-        self.lru = []        
+        self.cache = {}
+        self.capacity = capacity
+        self.head, self.tail = LinkedList(-1, -1), LinkedList(-1, -1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
 
+    def addNode(self, key, val):
+        node = LinkedList(key, val)
+        self.head.next.prev = node
+        node.next = self.head.next
+        self.head.next = node
+        node.prev = self.head
+        self.cache[key] = node
+        
+    def moveToHead(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+        node.next = self.head.next
+        node.prev = self.head
+        self.head.next.prev = node
+        self.head.next = node
+        
+    def popTail(self):
+        x = self.tail.prev
+        self.tail.prev = x.prev
+        x.prev.next = self.tail
+        return x
+        
     def get(self, key: int) -> int:
-        if key not in self.list: return -1
-        self.lru.remove(key)
-        self.lru.append(key)
-        return self.list[key]
+        if key not in self.cache: return -1
+        node = self.cache[key]
+        self.moveToHead(node)
+        return node.val
     
     def put(self, key: int, value: int) -> None:
-        self.list[key] = value
-        if key in self.lru:
-            self.lru.remove(key)
-            self.lru.append(key)
-        else: 
-            self.lru.append(key)
-        if len(self.list) > self.size:
-            self.list.pop(self.lru.pop(0))
+        if key in self.cache:
+            node = self.cache[key]
+            node.val = value
+            self.moveToHead(node)
+        else:
+            if self.capacity == len(self.cache):
+                self.cache.pop(self.popTail().key)
+            self.addNode(key, value)
+
         
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
