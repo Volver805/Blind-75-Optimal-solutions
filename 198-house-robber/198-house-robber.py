8291335589a0a2c0d72b9526747e0b59772274c5
Ahmed Sayed <37520614+Volver805@users.noreply.github.com>
2022-03-04 21:37:19 +0200

    Time: 38 ms (68.60%), Space: 13.9 MB (83.36%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,10 +1,6 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1: return nums[0]
-        @functools.lru_cache(maxsize = None)
-        def test(i, money=0):
-            if i >= len(nums):
-                return money
-            money += nums[i]
-            return max(test(i+2, money), test(i+3, money))
-        return max(test(0), test(1))
+        dp = [0] * (len(nums) + 3)
+        for i in reversed(range(len(nums))):
+            dp[i] = nums[i] + max(dp[i+2], dp[i+3])
+        return max(dp[0], dp[1])
