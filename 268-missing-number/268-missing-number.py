88b8cdf7ecbebe5f618e0d9d4f54e05cc15ed8bb
Ahmed Sayed <37520614+Volver805@users.noreply.github.com>
2022-03-05 23:51:16 +0200

    Time: 253 ms (27.09%), Space: 15.4 MB (42.11%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,6 +1,14 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        c = 0
-        for i in range(len(nums)+1):
-            c+=i
-        return c - sum(nums)
+        
+#         c = 0
+#         for i in range(len(nums)+1):
+#             c+=i
+#         return c - sum(nums)
+
+#       bit manipulation (optimal soultion)
+        res = len(nums)
+        for i, num in enumerate(nums):
+            print(bin(res))
+            res ^= i ^ num
+        return res
