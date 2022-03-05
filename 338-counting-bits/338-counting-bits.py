62c9c6cacb12e8b67d76d81ae8429ae742da8812
Ahmed Sayed <37520614+Volver805@users.noreply.github.com>
2022-03-05 23:33:46 +0200

    Time: 122 ms (55.97%), Space: 20.8 MB (83.75%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -1,11 +1,6 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        @functools.lru_cache()
-        def count1(b, c=0):
-            if b == 0:
-                return c
-            return count1(b & b-1, c+1)
-        res = [0]*(n+1)
-        for i in range(n+1):
-            res[i] = count1(i)
-        return res
+        res = [0] * (n + 1)
+        for i in range(1, n + 1):
+            res[i] = res[i & (i - 1)] + 1
+        return res 
