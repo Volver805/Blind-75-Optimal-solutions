f9fab1d19bc8d172eb7cebaafe3d20c0a9faf61b
Ahmed Sayed <37520614+Volver805@users.noreply.github.com>
2022-03-07 19:03:24 +0200

    Time: 70 ms (31.50%), Space: 14 MB (54.93%) - LeetHub

---
+++ 31-next-permutation/31-next-permutation.py
@@ -0,0 +1,20 @@
+class Solution:
+    def nextPermutation(self, nums) -> None :
+        br = False
+        i = len(nums)-1
+        while i != 0:
+            if nums[i] > nums[i-1]:
+                i-=1
+                br = True
+                break
+            i-=1
+        if not br:
+            nums.sort()
+            return
+        print(i)
+        j = len(nums)-1
+        while nums[j] <= nums[i]:
+            j -= 1
+        nums[i], nums[j] = nums[j], nums[i]
+        print(j)
+        nums[i+1:] = sorted(nums[i+1:])
